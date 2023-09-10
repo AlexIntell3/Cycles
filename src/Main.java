1041c810109f0142cf22e19d9488cc063310a2a9
AlexIntell3 <Pisarev81@inbox.u>
2023-09-10 20:29:00 +0200

    commit for HW Cycles Part 2

--- src/Main.java
+++ src/Main.java
@@ -3,60 +3,87 @@ public class Main {
 
         System.out.println("Задание 1");
 
-    for (int i=1;i<=10;i++){
-    System.out.println(i);}
+        int deposit = 15_000;
+        int accumulation = 0;
+        int i = 0;
+        while (accumulation <= 2_459_000) {
+            accumulation = accumulation + deposit;
+            System.out.println("Месяц " + i + " ,сумма накоплений равна " + accumulation + " рублей.");
+            i++;
+        }
 
-         System.out.println("Задание 2");
+        System.out.println("Задание 2");
+        int numbers = 1;
+        while (numbers <= 10) {
+            System.out.print(numbers + " ");
+            numbers++;
+        }
+        System.out.print("\n");
+        for (int numbers2 = 10; numbers2 >= 1; numbers2--) {
+            System.out.print(numbers2 + " ");
+        }
 
-    for (int i=10;i>0;i--){
-    System.out.println(i);}
+        System.out.print("\nЗадание 3\n");
 
-         System.out.println("Задание 3");
+        int population = 12_000_000;
+        int fertilityToThousand = 17;
+        int mortalityToThousand = 8;
+        int realFertilityToThousand = fertilityToThousand - mortalityToThousand;
+        int realFertilityTo12Million = population / 1000 * realFertilityToThousand;
+        int totalFertilityIncreaseToYear = 0;
+        for (int year = 1; year <= 10; year++) {
+            totalFertilityIncreaseToYear = totalFertilityIncreaseToYear + realFertilityTo12Million;
+            System.out.println("Год " + year + " численность населения сотавляет " + (totalFertilityIncreaseToYear + population) + " человек.");
+        }
 
-    for (int i=0;i<=17;i=i+2){
-    System.out.println(i);}
+        System.out.println("Задание 4");
 
-         System.out.println("Задание 4");
-    for (int i=10;i>=-10;i--){
-    System.out.println(i);}
+        int contribution = 15_000;
+        int stockpiling = 0;
+        int month = 1;
+        for (; stockpiling < 12_000_000; month++) {
+            stockpiling = stockpiling + contribution / 100 * 7;
+            System.out.println("Месяц " + month + " ,сумма накоплений равна " + (stockpiling + contribution) + " рублей.");
+        }
+
 
         System.out.println("Задание 5");
 
-    for (int i=1904;i<=2096;i=i+4){
-    System.out.println(i+" год является високосным");}
-
+        int contriBution = 15_000;
+        int stockPiling = 0;
+        int montH = 0;
+        for (; stockPiling < 12_000_000; montH++) {
+            stockPiling = stockPiling + contriBution / 100 * 7;
+            if (montH % 6 == 0) {
+                System.out.println("Месяц " + montH + " ,сумма накоплений равна " + (stockPiling + contriBution) + " рублей.");
+            }
+        }
         System.out.println("Задание 6");
 
-    for (int i=7;i<=98;i=i+7){
-    System.out.println(i);}
+        int investment = 15_000;
+        int savings = 0;
+        int moon = 1;
+        for (;moon<=108; moon++) {
+            savings = savings + investment / 100 * 7;
+            if (moon % 6 == 0) {
+                System.out.println("Месяц " + moon + " ,сумма накоплений равна " + (savings + investment) + " рублей.");}
+        }
 
         System.out.println("Задание 7");
-
-    for (int i=1;i<=512;i=i*2){
-    System.out.println(i);}
-
+        int friday=1;
+        for(int date=0;date<=31;date++){
+            if (date%7==friday){
+                System.out.println("Сегодня пятница "+date+"-е число.Необходимо подготовить отчет.");}
+            }
         System.out.println("Задание 8");
+        int period=79;
+        int currentYear=2023;
+        int startYear=currentYear -200;
+        int endYear=currentYear + 100;
+        for(int year=startYear;year<=endYear;year++){
+            if(year%period==0){
+                System.out.println(year);}
+            }
+        }
+    }
 
-    int moneyDeposit=29_000;
-    int totalMoneyAccumulation=0;
-    for (int i=1;i<=12;i++){
-        totalMoneyAccumulation=totalMoneyAccumulation+moneyDeposit;
-    System.out.println("Месяц "+i+" сумма накоплений равна "+totalMoneyAccumulation+" рублей.");}
-
-    System.out.println("Задание 9");
-
-    int deposit = 29_000;
-    int accumulation = 0;
-    for (int i = 1; i <= 12; i++) {
-        accumulation = accumulation + accumulation/100;
-        accumulation = accumulation + deposit;
-        System.out.println("Месяц " + i + " ,сумма накоплений равна " + accumulation+" рублей.");}
-
-        System.out.println("Задание 10");
-
-    int n=2;
-    for (int i = 1; i <= 10; i++){
-        System.out.println(n+"*"+i+"="+n*i);}
-
- }
-}
